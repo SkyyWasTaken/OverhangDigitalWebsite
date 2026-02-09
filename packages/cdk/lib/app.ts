cc88cacb5d2c995aaa20e8506097158417fbe95f
SkyyWasTaken <coolg0092002@gmail.com>
2026-02-08 20:40:10 -0500

    remove references to doggersdog

--- packages/cdk/lib/app.ts
+++ packages/cdk/lib/app.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import {DoggersDogPipelineStack} from "./pipeline-stack";
+import {OverhangPipelineStack} from "./pipeline-stack";
 import {ACCOUNTS, PIPELINE_ACCOUNT, PIPELINE_REGION, REGION} from "./constants";
 import {ApplicationStage} from "./application-stage";
 
 const app = new cdk.App();
 
-const pipelineStack = new DoggersDogPipelineStack(app, 'DoggersDogPipelineStack', {
+const pipelineStack = new OverhangPipelineStack(app, 'OverhangPipelineStack', {
     env: {
         account: PIPELINE_ACCOUNT,
         region: PIPELINE_REGION
